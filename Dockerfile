3281859480131e5bc66884f81d69fe0bbac125e5
Shankar Vellal <vidyashankar.mv@gmail.com>
2017-08-27 02:58:28 -0700

    HTTPS using Certbot

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,8 @@
 FROM golang
 
 ADD ./go/ .
+RUN go get golang.org/x/crypto/acme
 RUN go install main
 ENTRYPOINT /go/bin/main
 
-EXPOSE 8080
+EXPOSE 443
--- go/src/main/main.go
+++ go/src/main/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
+	"log"
 	"net/http"
+
+	"golang.org/x/crypto/acme/autocert"
 )
 
 func indexHandler (w http.ResponseWriter, r *http.Request) {
@@ -10,6 +14,23 @@ func indexHandler (w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	log.Println("starting")
+    
+    certManager := autocert.Manager{
+        Prompt:     autocert.AcceptTOS,
+        HostPolicy: autocert.HostWhitelist("shankarvellal.com"),
+        Cache:      autocert.DirCache("certs"), //folder for storing certificates
+    }
+
+    server := &http.Server{
+        Addr: ":443",
+        TLSConfig: &tls.Config{
+            GetCertificate: certManager.GetCertificate,
+        },
+    }
+
 	http.HandleFunc("/", indexHandler)
-	http.ListenAndServe(":8080", nil)
+	if err := server.ListenAndServeTLS("", ""); err != nil {
+        log.Fatalf(err.Error())
+    }
 }
